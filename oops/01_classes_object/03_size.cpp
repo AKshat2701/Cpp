a053496865186c35510fcc9d772b3658b617dff3
Akshat Srivastava <akshatsri2710@gmail.com>
2026-02-19 10:10:28 +0530

    size and static memory allocation

---
+++ oops/01_classes_object/03_size.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+class A 
+{
+    public:
+        char c;
+        int b ; 
+        
+
+
+};
+
+
+int main ()
+{   
+
+    A a ; 
+    cout<< "Size of " << sizeof(A) << endl;
+
+
+    return 0 ;
+}
